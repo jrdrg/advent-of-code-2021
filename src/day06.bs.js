779cca0ec5b71ab875e1f8ac294c0f5c2a646c2b
John Rodriguez <jrodriguez@spotify.com>
2021-12-07 23:55:21 -0500

    day 6

--- src/day06.bs.js
+++ src/day06.bs.js
@@ -4,92 +4,81 @@
 var List = require("rescript/lib/js/list.js");
 var $$Array = require("rescript/lib/js/array.js");
 var $$String = require("rescript/lib/js/string.js");
+var Caml_int64 = require("rescript/lib/js/caml_int64.js");
+var Belt_MapInt = require("rescript/lib/js/belt_MapInt.js");
 var Caml_format = require("rescript/lib/js/caml_format.js");
+
+var exampleInputStr = "3,4,3,1,2";
 
 var inputStr = "1,3,4,1,5,2,1,1,1,1,5,1,5,1,1,1,1,3,1,1,1,1,1,1,1,2,1,5,1,1,1,1,1,4,4,1,1,4,1,1,2,3,1,5,1,4,1,2,4,1,1,1,1,1,1,1,1,2,5,3,3,5,1,1,1,1,4,1,1,3,1,1,1,2,3,4,1,1,5,1,1,1,1,1,2,1,3,1,3,1,2,5,1,1,1,1,5,1,5,5,1,1,1,1,3,4,4,4,1,5,1,1,4,4,1,1,1,1,3,1,1,1,1,1,1,3,2,1,4,1,1,4,1,5,5,1,2,2,1,5,4,2,1,1,5,1,5,1,3,1,1,1,1,1,4,1,2,1,1,5,1,1,4,1,4,5,3,5,5,1,2,1,1,1,1,1,3,5,1,2,1,2,1,3,1,1,1,1,1,4,5,4,1,3,3,1,1,1,1,1,1,1,1,1,5,1,1,1,5,1,1,4,1,5,2,4,1,1,1,2,1,1,4,4,1,2,1,1,1,1,5,3,1,1,1,1,4,1,4,1,1,1,1,1,1,3,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,5,1,2,1,1,1,1,1,1,1,1,1";
 
 function parseInput(input) {
-  return List.map(Caml_format.caml_int_of_string, $$String.split_on_char(/* ',' */44, input));
+  return List.fold_left((function (fishCounts, next) {
+                return Belt_MapInt.update(fishCounts, next, (function (v) {
+                              if (v !== undefined) {
+                                return Caml_int64.add(v, Caml_int64.one);
+                              } else {
+                                return Caml_int64.one;
+                              }
+                            }));
+              }), undefined, List.map(Caml_format.caml_int_of_string, $$String.split_on_char(/* ',' */44, input)));
 }
 
-function updateFish(fishTimer) {
-  if (fishTimer === 0) {
-    return [
-            6,
-            8
-          ];
-  } else {
-    return [
-            fishTimer - 1 | 0,
-            undefined
-          ];
-  }
+function nextDay(fishCounts) {
+  return Belt_MapInt.reduce(fishCounts, undefined, (function (counts, timerValue, count) {
+                var inc = function (val) {
+                  if (val !== undefined) {
+                    return Caml_int64.add(val, count);
+                  } else {
+                    return count;
+                  }
+                };
+                if (timerValue > 0) {
+                  return Belt_MapInt.update(counts, timerValue - 1 | 0, inc);
+                } else {
+                  return Belt_MapInt.update(Belt_MapInt.update(counts, 6, inc), 8, inc);
+                }
+              }));
 }
 
-function passDay(fishes) {
-  var match = List.fold_left((function (param, next) {
-          var newFishes = param[1];
-          var origFishes = param[0];
-          var match = updateFish(next);
-          var newFish = match[1];
-          var updated = match[0];
-          if (newFish !== undefined) {
-            return [
-                    {
-                      hd: updated,
-                      tl: origFishes
-                    },
-                    {
-                      hd: newFish,
-                      tl: newFishes
-                    }
-                  ];
-          } else {
-            return [
-                    {
-                      hd: updated,
-                      tl: origFishes
-                    },
-                    newFishes
-                  ];
-          }
-        }), [
-        /* [] */0,
-        /* [] */0
-      ], fishes);
-  return List.append(List.rev(match[0]), List.rev(match[1]));
-}
-
-function passDays(_day, _fishes) {
+function nextNDays(_days, _fishCounts) {
   while(true) {
-    var fishes = _fishes;
-    var day = _day;
-    if (day <= 0) {
-      return fishes;
+    var fishCounts = _fishCounts;
+    var days = _days;
+    if (days === 0) {
+      return fishCounts;
     }
-    var updated = passDay(fishes);
-    _fishes = updated;
-    _day = day - 1 | 0;
+    _fishCounts = nextDay(fishCounts);
+    _days = days - 1 | 0;
     continue ;
   };
 }
 
 function part1(param) {
-  var input = parseInput(inputStr);
-  var out = $$Array.of_list(passDays(50, input));
-  console.log("OUT", out);
-  return out.length;
+  var input = parseInput(exampleInputStr);
+  var out = nextNDays(80, input);
+  return Caml_int64.to_float($$Array.fold_left((function (sum, param) {
+                    return Caml_int64.add(sum, param[1]);
+                  }), Caml_int64.zero, Belt_MapInt.toArray(out)));
 }
 
 console.log("Part 1", part1(undefined));
 
-var exampleInputStr = "3,4,3,1,2";
+function part2(param) {
+  var input = parseInput(inputStr);
+  var out = nextNDays(256, input);
+  return Caml_int64.to_float($$Array.fold_left((function (sum, param) {
+                    return Caml_int64.add(sum, param[1]);
+                  }), Caml_int64.zero, Belt_MapInt.toArray(out)));
+}
+
+console.log("Part 2", part2(undefined));
 
 exports.exampleInputStr = exampleInputStr;
 exports.inputStr = inputStr;
 exports.parseInput = parseInput;
-exports.updateFish = updateFish;
-exports.passDay = passDay;
-exports.passDays = passDays;
+exports.nextDay = nextDay;
+exports.nextNDays = nextNDays;
 exports.part1 = part1;
+exports.part2 = part2;
 /*  Not a pure module */
--- src/day06.res
+++ src/day06.res
@@ -3,49 +3,61 @@ let exampleInputStr = "3,4,3,1,2"
 let inputStr = "1,3,4,1,5,2,1,1,1,1,5,1,5,1,1,1,1,3,1,1,1,1,1,1,1,2,1,5,1,1,1,1,1,4,4,1,1,4,1,1,2,3,1,5,1,4,1,2,4,1,1,1,1,1,1,1,1,2,5,3,3,5,1,1,1,1,4,1,1,3,1,1,1,2,3,4,1,1,5,1,1,1,1,1,2,1,3,1,3,1,2,5,1,1,1,1,5,1,5,5,1,1,1,1,3,4,4,4,1,5,1,1,4,4,1,1,1,1,3,1,1,1,1,1,1,3,2,1,4,1,1,4,1,5,5,1,2,2,1,5,4,2,1,1,5,1,5,1,3,1,1,1,1,1,4,1,2,1,1,5,1,1,4,1,4,5,3,5,5,1,2,1,1,1,1,1,3,5,1,2,1,2,1,3,1,1,1,1,1,4,5,4,1,3,3,1,1,1,1,1,1,1,1,1,5,1,1,1,5,1,1,4,1,5,2,4,1,1,1,2,1,1,4,4,1,2,1,1,1,1,5,3,1,1,1,1,4,1,4,1,1,1,1,1,1,3,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,5,1,2,1,1,1,1,1,1,1,1,1"
 
 let parseInput = input => {
-  input |> String.split_on_char(',') |> List.map(int_of_string)
+  input
+  |> String.split_on_char(',')
+  |> List.map(int_of_string)
+  |> List.fold_left((fishCounts, next) => {
+    Belt.Map.Int.update(fishCounts, next, v => {
+      switch v {
+      | Some(count) => Some(Int64.add(count, Int64.of_int(1)))
+      | None => Some(Int64.of_int(1))
+      }
+    })
+  }, Belt.Map.Int.empty)
 }
 
-let updateFish = (fishTimer: int) => {
-  if fishTimer == 0 {
-    (6, Some(8))
-  } else {
-    (fishTimer - 1, None)
-  }
-}
+type fishCounts = Belt.Map.Int.t<int64>
 
-let passDay = (fishes: list<int>) => {
-  let (original, added) = fishes |> List.fold_left(((origFishes, newFishes), next) => {
-    let (updated, newFish) = updateFish(next)
-    switch newFish {
-    | Some(fish) => (list{updated, ...origFishes}, list{fish, ...newFishes})
-    | None => (list{updated, ...origFishes}, newFishes)
+let nextDay = (fishCounts: fishCounts) => {
+  Belt.Map.Int.reduce(fishCounts, Belt.Map.Int.empty, (counts, timerValue, count) => {
+    let inc = (val: option<int64>) =>
+      switch val {
+      | Some(v) => Some(Int64.add(v, count))
+      | None => Some(count)
+      }
+    if timerValue > 0 {
+      Belt.Map.Int.update(counts, timerValue - 1, inc)
+    } else {
+      Belt.Map.Int.update(counts, 6, inc)->Belt.Map.Int.update(8, inc)
     }
-  }, (list{}, list{}))
-
-  //   Js.log(original |> Array.of_list)
-  //   Js.log(added |> Array.of_list)
-
-  List.append(List.rev(original), List.rev(added))
+  })
 }
 
-let rec passDays = (day: int, fishes: list<int>) => {
-  if day > 0 {
-    let updated = passDay(fishes)
-    passDays(day - 1, updated)
+let rec nextNDays = (days: int, fishCounts: fishCounts) => {
+  if days == 0 {
+    fishCounts
   } else {
-    fishes
+    nextNDays(days - 1, nextDay(fishCounts))
   }
 }
 
 let part1 = () => {
-  let input = parseInput(inputStr)
+  let input = parseInput(exampleInputStr)
+  let out = nextNDays(80, input)
 
-  let out = passDays(50, input) |> Array.of_list
-
-  Js.log2("OUT", out)
-
-  Array.length(out)
+  Belt.Map.Int.toArray(out) |> Array.fold_left((sum, (_, count)) => {
+    Int64.add(sum, count)
+  }, Int64.of_int(0)) |> Int64.to_float
 }
 
 Js.log2("Part 1", part1())
+
+let part2 = () => {
+  let input = parseInput(inputStr)
+  let out = nextNDays(256, input)
+  Belt.Map.Int.toArray(out) |> Array.fold_left((sum, (_, count)) => {
+    Int64.add(sum, count)
+  }, Int64.of_int(0)) |> Int64.to_float
+}
+
+Js.log2("Part 2", part2())
